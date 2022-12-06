4ef0124e561812b409309a9c90dbfa826bb9dd02
JesusShocklogic <jesus@shocklogic.com>
2022-12-06 09:27:00 -0400

    Speakers page wordpress block is created. This can be used to create an speakers page automatically

--- template-parts/blocks/index.php
+++ template-parts/blocks/index.php
@@ -3,3 +3,4 @@ wp_register_style('shocklogic_banner', 'shocklogic_banner/shocklogic_banner.css'
 wp_register_style('shocklogic_single_iframe', 'shocklogic_single_iframe/shocklogic_single_iframe.css', array(), true);
 wp_register_style('shocklogic_blog', 'shocklogic_blog/shocklogic_blog.css', array(), true);
 wp_register_style('shocklogic_speakers_slider_wordpress', 'shocklogic_speakers_slider_wordpress/shocklogic_speakers_slider_wordpress.css', array(), true);
+wp_register_style('shocklogic_speakers_wordpress', 'shocklogic_speakers_wordpress/shocklogic_speakers_wordpress.css', array(), true);
