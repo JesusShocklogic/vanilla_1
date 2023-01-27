714a72c5a0e6f9abad15425fb36d2bc9f8da1419
JesusShocklogic <jesus@shocklogic.com>
2023-01-27 18:03:36 -0400

    Adding a full padding dashboard in to the footer.

--- footer.php
+++ footer.php
@@ -1,23 +1,40 @@
 <?php
 wp_enqueue_style("footer");
 $footer = get_field('general_footer_options', 'option');
-$number_of_columns = sizeof($footer['columns']); ?>
+$number_of_columns = sizeof($footer['columns']);
+$container = $footer['paddings'];
+
+$padding_mobile = $footer['padding_mobile'];
+$padding_tablet = $footer['padding_tablet'];
+$padding_desktop = $footer['padding_desktop']; ?>
 <style>
     footer {
         background-color: <?= $footer['background_colour'] ?>;
+    }
+
+    .footer {
+        padding: <?= $padding_mobile['top'] . $padding_mobile['unit'] . " " . $padding_mobile['right'] . $padding_mobile['unit'] . " " . $padding_mobile['bottom'] . $padding_mobile['unit'] . " " . $padding_mobile['left'] . $padding_mobile['unit'] ?>;
     }
 
     .footer_wrapper {
         grid-template-columns: 1fr;
     }
 
-    @media (min-width: 576px) {}
+    @media (min-width: 480px) {
+        .footer {
+            padding: <?= $padding_tablet['top'] . $padding_tablet['unit'] . " " . $padding_tablet['right'] . $padding_tablet['unit'] . " " . $padding_tablet['bottom'] . $padding_tablet['unit'] . " " . $padding_tablet['left'] . $padding_tablet['unit'] ?>;
+        }
+    }
 
     @media (min-width: 768px) {}
 
     @media (min-width: 992px) {}
 
     @media (min-width: 1200px) {
+        .footer {
+            padding: <?= $padding_desktop['top'] . $padding_desktop['unit'] . " " . $padding_desktop['right'] . $padding_desktop['unit'] . " " . $padding_desktop['bottom'] . $padding_desktop['unit'] . " " . $padding_desktop['left'] . $padding_desktop['unit'] ?>;
+        }
+
         .footer_wrapper {
             grid-template-columns: repeat(<?= $number_of_columns ?>, 1fr);
         }
@@ -27,7 +44,7 @@ $number_of_columns = sizeof($footer['columns']); ?>
     @media (min-width: 1400px) {}
 </style>
 
-<footer class="footer <?= $footer['spacing'] ?>">
+<footer class="footer <?= $container ?>">
     <div class="footer_wrapper">
         <?php
         if ($footer['columns']) {
