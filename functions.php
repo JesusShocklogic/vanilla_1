388b266e4f21490b2f767bbf5e7418cc4ac668b8
JesusShocklogic <jesus@shocklogic.com>
2023-02-23 10:52:36 -0400

    Adding shocklogic-category as a filter for the gutenberg block

--- functions.php
+++ functions.php
@@ -207,3 +207,23 @@ if (function_exists('acf_add_options_page')) {
         'parent_slug'    => 'theme-general-settings',
     ));
 } //ACF option menu in WP Dashboard
+
+/*
+* Adding a block category
+*/
+function filter_block_categories_when_post_provided($block_categories, $editor_context)
+{
+    if (!empty($editor_context->post)) {
+        array_push(
+            $block_categories,
+            array(
+                'slug'  => 'shocklogic-category',
+                'title' => __('Shocklogic'),
+                'icon'  => null,
+            )
+        );
+    }
+    return $block_categories;
+}
+
+add_filter('block_categories_all', 'filter_block_categories_when_post_provided', 10, 2);
