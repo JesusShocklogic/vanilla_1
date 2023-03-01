164c3ae44719e82978708d9abda5f1e5afcc6f9f
JesusShocklogic <jesus@shocklogic.com>
2023-03-01 14:25:37 -0400

    Social bar options added to switch from right to left and to push the bar away of the screen

--- template-parts/social-icons/social-icons-widget.php
+++ template-parts/social-icons/social-icons-widget.php
@@ -1,4 +1,12 @@
 <link rel="stylesheet" id="social-icon-widget-css" href="<?= get_template_directory_uri() ?>/template-parts/social-icons/social-icons-widget.css" type="text/css" media="all">
+<?php
+$distance_from_the_screen_edge = (isset($args['distance_from_the_screen_edge']) && $args['distance_from_the_screen_edge'] != "") ? $args['distance_from_the_screen_edge'] : "0"; ?>
+<style>
+    .shocklogic_social_media_bar {
+        background-color: <?= $args['background_colour'] ?? "rgba(32,32,32,0.5)"; ?>;
+        <?= $args['bar_position'] . ": " . $distance_from_the_screen_edge . ";"; ?>
+    }
+</style>
 <div class="shocklogic_social_media_bar">
     <div class="shocklogic_social_media_bar_container">
         <?php
