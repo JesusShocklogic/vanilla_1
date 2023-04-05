314ddeef1ef18a2a933170c837520f9b4be866eb
JesusShocklogic <jesus@shocklogic.com>
2023-04-05 19:25:11 -0400

    Removing word-break and adding word-wrap. Adding ACF features  to six column block

--- template-parts/blocks/six_column_table/six_column_table.php
+++ template-parts/blocks/six_column_table/six_column_table.php
@@ -12,12 +12,17 @@
  * @param   array $context The context provided to the block by the post or it's parent block.
  */
 $general_settings = get_field('general_settings');
+
 $spacing = $general_settings['spacing'];
 $background_colour = $general_settings['background_colour'];
 
 $block_id = $block['id'];
 $six_column_table_group = get_field('six_column_table_group');
 $table = $six_column_table_group['table'];
+$text_midle_alignment = (isset($six_column_table_group['text_midle_alignment']) && $six_column_table_group['text_midle_alignment'] == true) ? "align-middle" : "";
+
+$stripe_rows = (isset($six_column_table_group['stripe_rows']) && $six_column_table_group['stripe_rows'] == true) ? "table-striped" : "";
+$stripe_rows_backgrounds = (isset($six_column_table_group['stripe_rows_backgrounds'])) ? $six_column_table_group['stripe_rows_backgrounds'] : null;
 ?>
 <link rel="stylesheet" id="speakers-wordpress" href="<?= get_template_directory_uri() . "/template-parts/blocks/six_column_table/six_column_table.css" ?>" type="text/css" media="all">
 <style>
@@ -26,6 +31,10 @@ $table = $six_column_table_group['table'];
 	#$block_id{
 		background-color: $background_colour;
 	}
+
+	#$block_id .table-striped>tbody>tr:nth-of-type(odd)>*{
+		background-color: $stripe_rows_backgrounds;
+	}
 	ITEM;
 	echo $classes; ?>
 </style>
@@ -33,11 +42,11 @@ $table = $six_column_table_group['table'];
 	<?php
 	if ($table) : ?>
 		<div class="table-responsive">
-			<table class="table table-striped table-hover">
+			<table class="table table-hover <?= $text_midle_alignment . " " . $stripe_rows ?>">
 				<thead>
 					<tr>
 						<th scope="col" style="min-width: 50px;"> <?= $table[0]['col_1'] ?></th>
-						<th scope="col" style="min-width: 100px;"><?= $table[0]['col_2'] ?></th>
+						<th scope="col"><?= $table[0]['col_2'] ?></th>
 						<th scope="col" style="min-width: 100px;"><?= $table[0]['col_3'] ?></th>
 						<th scope="col" style="min-width: 300px;"><?= $table[0]['col_4'] ?></th>
 						<th scope="col" style="min-width: 200px;"><?= $table[0]['col_5'] ?></th>
