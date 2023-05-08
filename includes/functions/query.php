a685af85c78c4ba04d5fc53ebea13ec87b4da658
JesusShocklogic <jesus@shocklogic.com>
2023-05-08 10:55:48 -0400

    Bug in the query.php

--- includes/functions/query.php
+++ includes/functions/query.php
@@ -6,27 +6,27 @@ function get_query($args)
 
     $post_type = (isset($args['post_types']) && $args['post_types'] != "") ? $args['post_types'] : "post";
     $cat = (isset($args['post_categories'])) ? $args['post_categories'] : null;
-    $posts_per_page = isset($args['posts_per_page']) ? $args['posts_per_page'] : 3;
-    if($posts_per_page === 0 || $posts_per_page === "") $posts_per_page = -1;
-    $orderby = isset($args['orderby']) ? $args['orderby'] : "date";
-    $order = isset($args['order']) ? $args['order'] : "DESC";
 
     if (isset($args['posts_per_page'])) {
-        if ($args['posts_per_page'] == 0) {
-            $posts_per_page = "";
+        if ($args['posts_per_page'] <= 0) {
+            $posts_per_page = "-1";
         } else {
             $posts_per_page = $args['posts_per_page'];
         }
     }
+    $orderby = isset($args['orderby']) ? $args['orderby'] : "date";
+    $order = isset($args['order']) ? $args['order'] : "DESC";
 
     $query_args = array(
-        'post_type'         => $post_type,
-        'posts_per_page'   => $posts_per_page,
+        'post_type' => $post_type,
+        'posts_per_page' => $posts_per_page,
         'cat' => $cat,
-        'offset'            => 0,
-        'post_status'      => 'publish',
-        'orderby'           => $orderby, // for example'orderby' => 'name'
-        'order'             => $order, // ASC ascended , DESC descend
+        'offset' => 0,
+        'post_status' => 'publish',
+        'orderby' => $orderby,
+        // for example'orderby' => 'name'
+        'order' => $order,
+        // ASC ascended , DESC descend
         'post__not_in' => array(get_the_ID()), //Avoid showing current post
     );
 
