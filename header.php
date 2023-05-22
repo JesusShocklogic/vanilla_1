fbd8bda8941169ed706121a62c5af17c7c44a996
JesusShocklogic <jesus@shocklogic.com>
2023-05-22 11:13:06 -0400

    adjustments in the menu

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php is_front_page() ? the_title() : wp_title(''); ?></title>
+	<title>
+		<?php is_front_page() ? the_title() : wp_title(''); ?>
+	</title>
 
 	<?php
 	wp_head();
@@ -31,50 +33,69 @@
 	?>
 	<style>
 		a {
-			color: <?= $general_theme_settings['general_links_colour'] ?? "auto" ?>;
+			color:
+				<?= $general_theme_settings['general_links_colour'] ?? "auto" ?>
+			;
 			text-decoration: none;
 		}
 
 		a:hover {
-			color: <?= $general_theme_settings['general_links_colour_hover'] ?? "auto" ?>;
+			color:
+				<?= $general_theme_settings['general_links_colour_hover'] ?? "auto" ?>
+			;
 		}
 
 		header .navbar {
-			background-color: <?= $general_menu_group['background_colour'] ?>;
+			background-color:
+				<?= $general_menu_group['background_colour'] ?>
+			;
 			box-shadow: 0px 5px 6px #00000029;
 		}
- 
+
 		header .navbar .menu-item {
 			width: max-content;
 		}
+
 		header .navbar .menu-item .nav-link {
-			color: <?= $general_menu_group['menu_items_text_colour'] ?>;
+			color:
+				<?= $general_menu_group['menu_items_text_colour'] ?>
+			;
 		}
 
 		header .navbar .menu-item .nav-link:hover {
-			color: <?= $general_menu_group['menu_items_text_colour_hover'] ?>;
+			color:
+				<?= $general_menu_group['menu_items_text_colour_hover'] ?>
+			;
 		}
 
 		header .navbar .menu-item .nav-link.active {
-			color: <?= $general_menu_group['active_menu_item'] ?>;
+			color:
+				<?= $general_menu_group['active_menu_item'] ?>
+			;
 		}
 
 		/* Sizes for the menu logo */
 		header a.navbar-brand img {
-			width: <?= $general_menu_group['logo_mobile_width'] . "%" ?? "100%"; ?>;
+			width:
+				<?= $general_menu_group['logo_mobile_width'] . "%" ?? "100%"; ?>
+			;
 		}
 
 		/* tablet */
 		@media (min-width: 769px) {
 			header a.navbar-brand img {
-				width: <?= $general_menu_group['logo_tablet_width'] . "%" ?? "100%"; ?>;
+				width:
+					<?= $general_menu_group['logo_tablet_width'] . "%" ?? "100%"; ?>
+				;
 			}
 		}
 
 		/* Desktop */
 		@media (min-width: 1200px) {
 			header a.navbar-brand img {
-				width: <?= $general_menu_group['logo_desktop_width'] . "%" ?? "100%"; ?>;
+				width:
+					<?= $general_menu_group['logo_desktop_width'] . "%" ?? "100%"; ?>
+				;
 			}
 		}
 	</style>
@@ -82,25 +103,31 @@
 	<header>
 		<nav class="navbar navbar-expand-xl <?= $general_menu_group['menus_position'] ?>">
 			<div class="container-fluid">
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+					data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
+					aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-					<a class="navbar-brand" href="<?= get_home_url() ?>">
-						<img src="<?= $general_menu_group['logo']['url'] ?>" alt="">
-					</a>
+					<div>
+						<a class="navbar-brand" href="<?= get_home_url() ?>">
+							<img src="<?= $general_menu_group['logo']['url'] ?>" alt="">
+						</a>
+					</div>
 					<?php
 					//Printing the menu
-					wp_nav_menu(array(
-						'theme_location' => 'main-menu',
-						'menu_id' => 'main-menu',
-						'container' => false,
-						'menu_class' => 'navbar-nav ms-auto mb-2 mb-md-0',
-						'fallback_cb' => '__return_false',
-						'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-						'depth' => 2,
-						'walker' => new bootstrap_5_wp_nav_menu_walker()
-					));
+					wp_nav_menu(
+						array(
+							'theme_location' => 'main-menu',
+							'menu_id' => 'main-menu',
+							'container' => false,
+							'menu_class' => 'navbar-nav ms-auto mb-2 mb-md-0',
+							'fallback_cb' => '__return_false',
+							'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+							'depth' => 2,
+							'walker' => new bootstrap_5_wp_nav_menu_walker()
+						)
+					);
 					?>
 					<?php
 					//Printing the right menu button
